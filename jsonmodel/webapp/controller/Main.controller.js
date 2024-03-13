560785272d6451b4a213182ea7fb46dda01eb0d2
도현 <kdh427@konkuk.ac.kr>
2024-03-13 18:13:28 +0900

    체크박스에 따른 input 활성화/비활성화

--- jsonmodel/webapp/controller/Main.controller.js
+++ jsonmodel/webapp/controller/Main.controller.js
@@ -1,14 +1,28 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel" // 화면구현을 위한 정의
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("sync.e05.jsonmodel.controller.Main", {
             onInit: function () {
+                let data = {
+                    firstName: "John",  // 문자열
+                    enabled: true       // boolean 타입
+                };
+
+                // sap.ui.mode.json.JSONModel 의 객체가 생성되면서
+                // 동시에 data 변수에 기록된 Structure 정보가 
+                // Model 의 데이터로 전달됨
+                let oModel = new JSONModel(data);
+
+                // 이 Controller 와 연결된 View 의 기본 모델로 설정
+                this.getView().setModel(oModel);
 
             }
         });
