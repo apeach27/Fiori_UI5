cb07d8f9d00776bf4e9cb3590c47f13ce8069a74
도현 <kdh427@konkuk.ac.kr>
2024-03-13 17:19:25 +0900

    Input 추가

--- exercise8/webapp/controller/Main.controller.js
+++ exercise8/webapp/controller/Main.controller.js
@@ -17,6 +17,7 @@ sap.ui.define([
 
                 let oModel = new JSONModel(sPerson);
                 let oView = this.getView();
+               
                 oView.setModel(oModel); // JSON Model 정보를 현재 View 에 등록
             },
 
@@ -27,8 +28,16 @@ sap.ui.define([
                 // Popup.fragment.xml 을 팝업창으로 호출함
                 let oView = this.getView();
                 let oDialog = this.byId("idDialog");
+                let oInput = this.byId("idInput");  // View 에서 idInput 인 ID 를 가진 Element 를 찾음
+                let name = oInput.getValue();      // 해당 Element 의 getValue 에 메소드를 실행함
 
                 if( oDialog ){
+
+                    // 팝업창의 텍스트 Element 중 ID 가 idNameText 인 항목을 찾아서
+                    let oNameText = this.byId("idNameText");
+                    // 이름이 입력된 입력 필드의 값을 전달함
+                    oNameText.setText(name);   
+
                     oDialog.open(); // Fragment 를 Load 한 적 있으면 byId 로 찾아라
                                     // 그렇게 찾은 Fragment 를 팝업으로 보여줌
                 } else {
@@ -46,6 +55,9 @@ sap.ui.define([
                     // (Main View 의 모델도 이용 가능)
                     oFragment.then( function( oDialog ){
                         oView.addDependent(oDialog)  // View 에 연결
+
+                        let oNameText = oView.byId("idNameText");
+                        oNameText.setText(name);
                         oDialog.open();  // 팝업창 출력
                     });
                 }
