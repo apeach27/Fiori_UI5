b97f8058cc00a7f2a91c5e759566e6baaefc6ca6
도현 <kdh427@konkuk.ac.kr>
2024-03-13 17:54:11 +0900

    JSON Model 파일 생성

---
+++ jsonmodel/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sync.e05.jsonmodel.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ jsonmodel/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synce05/jsonmodel/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
