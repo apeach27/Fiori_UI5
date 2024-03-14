b3d928d9109a3737ecf3155d2e3e801c4b21c12f
도현 <kdh427@konkuk.ac.kr>
2024-03-14 21:20:25 +0900

    task4 팝업기능까지 구현

--- ze05_gw_hw1/webapp/controller/Main.controller.js
+++ ze05_gw_hw1/webapp/controller/Main.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -10,6 +11,56 @@ sap.ui.define([
         return Controller.extend("ui5tablefragment.ze05gwhw1.controller.Main", {
             onInit: function () {
 
+            },
+
+            onSelectionChange: function( oEvent ){
+
+                // 선택한 Item 정보를 Event 로 부터 가져옴
+                let onItem = oEvent.getParameter("listItem");
+
+                // 선택한 라인에서 출력되는 데이터의 Model의 값 정보
+                let oContext = onItem.getBindingContext();
+
+                let carrid = oContext.getProperty("Carrid");
+                let connid = oContext.getProperty("Connid");
+
+                // 해당 Model 내용의 Carrid 속성을 화면에 출력
+                sap.m.MessageToast.show("선택 라인은 항공사: "+carrid+ "항공편: "+connid +" 의 정보입니다.");
+
+                
+                // Popup.fragment.xml 을 팝업창으로 호출함
+                let oView = this.getView();
+                let oDialog = this.byId("idDialog");
+
+                if( oDialog ){
+
+                    oDialog.open(); // Fragment 를 Load 한 적 있으면 byId 로 찾아라
+                                    // 그렇게 찾은 Fragment 를 팝업으로 보여줌
+                } else {
+                    // Popup.fragment.xml 파일을 읽어오기
+                    // Controller 도 연결함
+                    let oFragment = sap.ui.core.Fragment.load({
+                        id: oView.getId(),
+                        name: "ui5tablefragment.ze05gwhw1.view.Info",
+                        type: "XML",
+                        controller: this
+
+                    });
+
+                    // Fragment Load 가 완료되면 Main View 에 연결함
+                    // (Main View 의 모델도 이용 가능)
+                    oFragment.then( function( oDialog ){
+                        oView.addDependent(oDialog)  // View 에 연결
+                        oDialog.open();  // 팝업창 출력
+                    });
+                }
+            },
+            
+            onDialogClose: function(){
+                let oDialog = this.byId("idDialog");
+                if(oDialog){
+                    oDialog.close(); //  팝업창 닫기
+                }
             }
         });
     });
