95b6e9a88851b604f8ed07d373c511b08b19e09e
도현 <kdh427@konkuk.ac.kr>
2024-03-15 15:37:43 +0900

    hw1 삭제

--- homework1/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["sync/e05/homework1/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- homework1/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"synce05/homework1/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
