374f1992af2b567848fd4d490d618cebc55d9fc6
도현 <kdh427@konkuk.ac.kr>
2024-03-18 10:54:13 +0900

    [add] review폴더 생성

---
+++ hw1review/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/e05/hw1review/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ hw1review/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synce05/hw1review/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
