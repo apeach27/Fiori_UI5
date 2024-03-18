26061c3dd32a1a0374928dc22970ae0354a3f2cd
도현 <kdh427@konkuk.ac.kr>
2024-03-18 12:07:26 +0900

    [modify] MessageToast 띄우기

--- hw1review/webapp/controller/Main.controller.js
+++ hw1review/webapp/controller/Main.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("sync.e05.hw1review.controller.Main", {
@@ -12,10 +13,17 @@ sap.ui.define([
 
             },
             onSelectionChange: function( oEvent ){
-                let listItem = oEvent.getParameter("listItem")
-                alert(listItem.getBindingContext())
-                let context = listItem.getBindingContext()
-                alert(context.getProperty("Carrid"));
+                // let listItem = oEvent.getParameter("listItem")
+                // alert(listItem.getBindingContext())
+                // let context = listItem.getBindingContext()
+                // alert(context.getProperty("Carrid"));
+                let oListItem = oEvent.getParameter("listItem");
+                let oContext = oListItem.getBindingContext();
+                let carrid = oContext.getProperty("Carrid");
+                let connid = oContext.getProperty("Connid");
+
+                MessageToast.show("선택하신 라인은 항공사: "+carrid+", 항공편: "+connid+" 의 정보입니다.");
+
             }
         });
     });
