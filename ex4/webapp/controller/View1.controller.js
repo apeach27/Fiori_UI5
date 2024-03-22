34ae0da71f858c0de48257eb788784127306431a
도현 <kdh427@konkuk.ac.kr>
2024-03-22 17:36:50 +0900

    UI5. 수정모드 버튼 생성

--- ex4/webapp/controller/View1.controller.js
+++ ex4/webapp/controller/View1.controller.js
@@ -1,15 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("sync.e05.ex4.controller.View1", {
             onInit: function () {
+                let viewData = {
+                    EditMode: false
+                };
 
+                let oViewModel = new JSONModel(viewData);
+                let oView = this.getView();
+                oView.setModel(oViewModel, "view");
+
+            },
+
+            onEdit: function(){
+                let oViewModel =  this.getView().getModel("view");
+                oViewModel.setProperty("/EditMode", true);
+            },
+
+            onCancel: function(){
+                let oViewModel =  this.getView().getModel("view");
+                oViewModel.setProperty("/EditMode", false);
+
+                let oModel = this.getView().getModel();
+                oModel.resetChanges();
             }
         });
     });
