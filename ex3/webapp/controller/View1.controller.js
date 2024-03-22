a6bac6e159a3c641e5f27616dcef950740a65025
도현 <kdh427@konkuk.ac.kr>
2024-03-22 17:20:36 +0900

    UI5. ex3 생성/삭제 기능

--- ex3/webapp/controller/View1.controller.js
+++ ex3/webapp/controller/View1.controller.js
@@ -11,13 +11,6 @@ sap.ui.define([
 
         return Controller.extend("sync.e05.ex3.controller.View1", {
 
-            initAirlineData: {
-                Carrid: "",
-                Carrname: "",
-                Currcode: "",
-                Url: "",
-            },
-
             onInit: function () {
                 let data = this.initAirlineData;
                 let oNewModel = new JSONModel(data);
@@ -55,12 +48,81 @@ sap.ui.define([
                     oDialog.close();
                 }
 
-                // 빈값만 있는 정보를 새로운 JSONModel을 만들어서 기존 new모델을 교체함
-                // --> 데이터 초기화
-                let oNewModel = new JSONModel(this.initAirlineData);
-                this.getView().setModel(oNewModel, "new");
-            }
+                let oNewModel = this.getView().getModel("new");
+                oNewModel.setData({
+                    Carrid: "",
+                    Carrname: "",
+                    Currcode: "",
+                    Url: ""
+                })
 
+                // ComboBox 선택된 내용을 지우는 과정
+                let oComboBox = this.byId("idComboBox");
+                oComboBox.setSelectedKey("");
+                oComboBox.setSelectedItem("");
+                oComboBox.setSelectedItemId("");
+            },
+
+            onSaveConfirm: function(){
+                let oView = this.getView();
+                let oNewModel = oView.getModel("new");  // JSON Model
+                let oModel = oView.getModel();          // oData Model            
+                let newData = oNewModel.getData();
+
+
+                oModel.create(
+                    // 경로, 신규데이터, 결과처리
+                    "/ConnectionSet",
+                    newData,
+                    {
+                        success:function(oData, oResponse){
+                            sap.m.MessageToast.show(oData.Carrid + "항공사가 생성되었습니다.");
+                        },
+
+                        error: function(oError){
+                            sap.m.MessageBox.error("생성 중 오류가 발생했습니다.");
+                        }
+                    }
+
+                );
+                
+                //생성을 위한 팝업창 닫기
+                this.onSaveCancel();
+
+            },
+
+            onDelete: function(){
+                let oTable =  this.byId("idTable"); 
+                console.log(oTable);
+                let aIndex = oTable.getSelectedIndices();
+
+                if (!aIndex || aIndex.length == 0){ // 선택된 항목이 없을 때
+                    sap.m.MessageBox.information("삭제할 라인을 선택하세요");
+                    return; // 메세지 출력 후 중단
+
+                }
+                let oModel =  this.getView().getModel();
+
+                for( const index of aIndex ){
+                    // 선택된 라인들 중 순서대로 하나씩 모델 연결 정보를 가져
+                    let oSelectedContext = oTable.getContextByIndex(index);
+                    let carrid = oSelectedContext.getProperty("Carrid");
+
+                    // 해당 모델의 정보에 대한 경로
+                    let path =  oSelectedContext.getPath();
+                    oModel.remove(path, {
+                        success: function(){
+                            // Exception 이 발생하지 않은 경우
+                            sap.m.MessageBox.success( carrid + "삭제 성공");
+                            oTable.clearSelection();                            
+
+                        },
+                        error: function(oError){
+                            sap.m.MessageBox.error("삭제 오류 발생");
+                        }
+                    });
+                }
+            }
 
         });
     });
