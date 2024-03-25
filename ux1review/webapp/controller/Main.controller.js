e5fda7fd51d3b9e5129c39e76ec548e996d08599
도현 <kdh427@konkuk.ac.kr>
2024-03-25 11:08:05 +0900

    UI5 1번 리뷰. 사칙연산

--- ux1review/webapp/controller/Main.controller.js
+++ ux1review/webapp/controller/Main.controller.js
@@ -24,6 +24,72 @@ sap.ui.define([
 
                 let oMessageManager = sap.ui.getCore().getMessageManager();
                 oMessageManager.registerObject(this.getView(), true);
-            }
+            },
+
+            onAdd: function(){
+                sap.m.MessageToast.show("덧셈 계산");
+
+                let oView = this.getView();
+                let oModel = oView.getModel();
+
+                let num1 = parseFloat(oModel.getProperty("/num1"));
+
+                let onInput2 = oView.byId("idNum2Input");
+                let num2 = parseFloat(onInput2.getValue());
+
+                let result = num1 + num2;
+                oModel.setProperty("/add", result);
+            },
+            
+            onSubtract: function(){
+                sap.m.MessageToast.show("뺄셈 계산");
+
+                let oView = this.getView();
+                let oModel = oView.getModel();
+
+                let num1 = parseFloat(oModel.getProperty("/num1"));
+
+                let onInput2 = oView.byId("idNum2Input");
+                let num2 = parseFloat(onInput2.getValue());
+
+                let result = num1 - num2;
+                oModel.setProperty("/subtract", result);
+            },
+            
+            onMulti: function(){
+                sap.m.MessageToast.show("곱셈 계산");
+
+                let oView = this.getView();
+                let oModel = oView.getModel();
+
+                let num1 = parseFloat(oModel.getProperty("/num1"));
+
+                let onInput2 = oView.byId("idNum2Input");
+                let num2 = parseFloat(onInput2.getValue());
+
+                let result = num1 * num2;
+                oModel.setProperty("/multi", result);
+            },
+            
+            onDivide: function(){
+                sap.m.MessageToast.show("나눗셈 계산");
+
+                let oView = this.getView();
+                let oModel = oView.getModel();
+
+                let num1 = parseFloat(oModel.getProperty("/num1"));
+
+                let onInput2 = oView.byId("idNum2Input");
+                let num2 = parseFloat(onInput2.getValue());
+
+                if(num2 == 0){
+                    sap.m.MessageBox.error('0으로 나눌 수 없음');
+                    return;
+                }
+
+                let result = num1 / num2;
+                oModel.setProperty("/divide", result);
+            },
+            
         });
     });
