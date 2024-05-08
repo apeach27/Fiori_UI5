bd5575bd35e6c2f46edcd643e2e6eba256594c34
도현 <kdh427@konkuk.ac.kr>
2024-05-08 17:00:33 +0900

    modify: Detail 상단에 title 생성

--- fcl/webapp/controller/Detail.controller.js
+++ fcl/webapp/controller/Detail.controller.js
@@ -13,13 +13,14 @@ sap.ui.define([
 			this.oRouter.getRoute("master").attachPatternMatched(this._onCarrierMatched, this);
 			this.oRouter.getRoute("detail").attachPatternMatched(this._onCarrierMatched, this);
 		},
-
-		_onCarrierMatched: function (oEvent) {
-			this._carrid = oEvent.getParameter("arguments").carrid || this._carrid || "";
-			this.getView().bindElement({
-				path: "/CarrierSet(" + this._carrid + ")"
-			});
-		},
+        
+        _onCarrierMatched: function (oEvent) {
+            this._carrid = oEvent.getParameter("arguments").carrid || this._carrid || "";
+            
+            // this._carrid = AA
+            // /CarrierSet(' + this._carrid + ') => /CarrierSet('AA')
+            this.getView().bindElement("/CarrierSet('" + this._carrid + "')");
+        },
 		onEditToggleButtonPress: function() {
 			var oObjectPage = this.getView().byId("ObjectPageLayout"),
 				bCurrentShowFooterState = oObjectPage.getShowFooter();
@@ -29,14 +30,6 @@ sap.ui.define([
 		onExit: function () {
 			this.oRouter.getRoute("master").detachPatternMatched(this._onCarrierMatched, this);
 			this.oRouter.getRoute("detail").detachPatternMatched(this._onCarrierMatched, this);
-		},
-
-		_onProductMatched: function (oEvent) {
-			this._product = oEvent.getParameter("arguments").product || this._product || "0";
-			this.getView().bindElement({
-				path: "/ProductCollection/" + this._product,
-				model: "products"
-			});
-		},
+		}
 	});
 });
